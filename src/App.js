4e9d5c574550ac7634930860a73d65c5592becd9
ndkv9 <vu.nguyen101@gmail.com>
2021-09-23 21:26:54 +0300

    define loginService

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs => setBlogs(blogs))
   }, [])
+
+  const handleLogin = event => {
+    event.preventDefault()
+    console.log('logged in with', username, password)
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+
+      <LoginForm
+        username={username}
+        password={password}
+        setUsername={setUsername}
+        setPassword={setPassword}
+        handleLogin={handleLogin}
+      />
+
+      {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
