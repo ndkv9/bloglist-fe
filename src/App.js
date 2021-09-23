8963c6200d355d4866e9f10929270b4fb4bf5263
ndkv9 <vu.nguyen101@gmail.com>
2021-09-23 21:33:40 +0300

    implement handleLogin

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs => setBlogs(blogs))
   }, [])
 
-  const handleLogin = event => {
+  const handleLogin = async event => {
     event.preventDefault()
-    console.log('logged in with', username, password)
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
   }
 
   return (
