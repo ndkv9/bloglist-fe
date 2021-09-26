8f634556c0af3305bc83457cb66a3160b2fad174
ndkv9 <vu.nguyen101@gmail.com>
2021-09-26 20:12:54 +0300

    implement toggle blog view

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BlogList = ({ blogs }) => {
   return (
@@ -10,10 +10,43 @@ const BlogList = ({ blogs }) => {
   )
 }
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(null)
+
+  const hideWhenVisibility = { display: visible ? 'none' : '' }
+  const showWhenVisibility = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    backgroundColor: '#63B4B8',
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisibility}>
+        <p>
+          {blog.title} {blog.author}{' '}
+          <button onClick={toggleVisibility}>view</button>
+        </p>
+      </div>
+      <div style={showWhenVisibility}>
+        <p>
+          {blog.title} {blog.author}{' '}
+          <button onClick={toggleVisibility}>hide</button>
+        </p>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes}</p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default BlogList
